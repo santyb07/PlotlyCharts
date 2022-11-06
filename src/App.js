f0b8b2959d3224e7993aa434c205115fce2ee1f4
Santosh Bhosale <santoshbhosale3231@gmail.com>
2022-11-06 11:44:43 +0530

    bubble chart for positive, negative and neutral influencers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
 //Plotly charts
-import BarChart from './BarChart';
+// import BarChart from './BarChart';
 import BubbleChart from './BubbleChart';
-import PieCharts from './PieCharts';
-import ScatteredChart from './ScatteredChart';
+// import PieCharts from './PieCharts';
+// import ScatteredChart from './ScatteredChart';
 
 import axios from "axios"
 
@@ -19,44 +19,85 @@ const App = () => {
     }
     fetchStats()
   },[])
+
   console.log(instagramDailyInfluencerStats)
-  let dates= []
-  dates=instagramDailyInfluencerStats?.instagram?.timelineStats?.dailyInfluencers?.map((data,index)=>{
+  let instagramDailyInfluencersDate= []
+  instagramDailyInfluencersDate=instagramDailyInfluencerStats?.instagram?.timelineStats?.dailyInfluencers?.map((data,index)=>{
     // console.log(data.date.split('T')[0])
     return data.date.split('T')[0];
   })
+
+  let sortedInstagramDailyInfluencersDate=[]
+  sortedInstagramDailyInfluencersDate=instagramDailyInfluencersDate?.reverse()
+  // console.log(sortedInstagramDailyInfluencersDate)
+
+  let instagramDailyInfluencersTotal= []
+  instagramDailyInfluencersTotal = instagramDailyInfluencerStats?.instagram?.timelineStats?.dailyInfluencers?.map((data,index)=>{
+    // const influencersTotal= "influencers Total :"+data.influencersTotal
+    // console.log(influencersTotal)
+    return data.influencersTotal;
+  })
+  let instagramDailyPositiveInfluencers= [];
+  instagramDailyPositiveInfluencers = instagramDailyInfluencerStats?.instagram?.timelineStats?.dailyInfluencers?.map((data,index)=>{
+    // const positiveInfluencers="Positive influencers Total :"+data.positiveInfluencersTotal
+    // console.log(positiveInfluencers)
+    return data.positiveInfluencersTotal
+  })
+  let instagramDailyNegativeInfluencers= [];
+  instagramDailyNegativeInfluencers = instagramDailyInfluencerStats?.instagram?.timelineStats?.dailyInfluencers?.map((data,index)=>{
+    // const negativeInfluencers="Negative influencers Total :"+data.negativeInfluencersTotal
+    // console.log(negativeInfluencers)
+    return data.negativeInfluencersTotal
+  })
+
+  let instagramDailyNeutralInfluencers= [];
+  instagramDailyNeutralInfluencers = instagramDailyInfluencerStats?.instagram?.timelineStats?.dailyInfluencers?.map((data,index)=>{
+    // const neutralInfluencers="Neutral influencers Total :"+data.neutralInfluencersTotal
+    // console.log(neutralInfluencers)
+    return data.neutralInfluencersTotal
+  })
+
+  // console.log("total:",instagramDailyInfluencersTotal)
+  // console.log("positive:",instagramDailyPositiveInfluencers)
+  // console.log("neutral:",instagramDailyNegativeInfluencers)
+  // console.log("negative:",instagramDailyNeutralInfluencers)
   // console.log(dates)
 
-  let dailyInfluencersTotal= []
-  dailyInfluencersTotal = instagramDailyInfluencerStats?.instagram?.timelineStats?.dailyInfluencers?.map((data,index)=>{
-    const influencersTotal= "influencers Total :"+data.influencersTotal
-    console.log(influencersTotal)
-    // return [...dailyInfluencersTotal,JSON.stringify(data.influencersTotal)]
-  })
+  const sortedInstagramDailyInfluencersStats = {
+    sortedInstagramDailyInfluencersDate,
+    instagramDailyInfluencersTotal,
+    instagramDailyPositiveInfluencers,
+    instagramDailyNegativeInfluencers,
+    instagramDailyNeutralInfluencers,
+  }
 
 
 
 
   
     return (
-      <div className='charts'>
-        <div>
-          <h3>Barchart</h3>
-          <BarChart/>
-        </div>
-        <div>
-        <h3>Bubble chart</h3>
-          <BubbleChart/>
-        </div>
-        <div>
-        <h3>Pie chart</h3>
-          <PieCharts/>
-        </div>
-        <div>
-        <h3>Scattered chart</h3>
-          <ScatteredChart/>
-        </div>
+      // <div className='charts'>
+      //   <div>
+      //     <h3>Barchart</h3>
+      //     <BarChart/>
+      //   </div>
+      //   <div>
+      //   <h3>Bubble chart</h3>
+      //     <BubbleChart/>
+      //   </div>
+      //   <div>
+      //   <h3>Pie chart</h3>
+      //     <PieCharts/>
+      //   </div>
+      //   <div>
+      //   <h3>Scattered chart</h3>
+      //     <ScatteredChart/>
+      //   </div>
+      // </div>
+      <div style={{width:'100%',height:'100%'}}>
+        <BubbleChart sortedInstagramDailyInfluencersStats={sortedInstagramDailyInfluencersStats}/>
       </div>
+
     );
   
 }
--- src/BubbleChart.jsx
+++ src/BubbleChart.jsx
@@ -1,26 +1,105 @@
 import React from 'react'
 import Plot from 'react-plotly.js';
 
-const BubbleChart = () => {
-    var trace1 = {
-        x: [1, 2, 3, 4],
-        y: [5,10,15,20,25,30,35,40,45,50,55,60,65],
-        text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-        mode: 'markers',
-        marker: {
-            // color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-            size: [40, 60, 80, 100]
-        }
-      };
-      
-      var data = [trace1];
-      
-      var layout = {
-        title: 'Marker Size',
-        showlegend: false,
-        height: 500,
-        width: 500
-      };      
+const BubbleChart = (props) => {
+
+  // console.log(props.sortedInstagramDailyInfluencersStats.instagramDailyNegativeInfluencers)
+
+
+  let instagramDailyNeutralInfluencersText= [];
+  instagramDailyNeutralInfluencersText = props?.sortedInstagramDailyInfluencersStats?.instagramDailyNeutralInfluencers?.map((value,index)=>{
+    const neutralInfluencersText="Neutral influencers Total :"+ value
+    // console.log(neutralInfluencersText)
+    return neutralInfluencersText
+  })
+
+  let instagramDailyNegativeInfluencersText= [];
+  instagramDailyNegativeInfluencersText = props?.sortedInstagramDailyInfluencersStats?.instagramDailyNegativeInfluencers?.map((value,index)=>{
+    const negativeInfluencers="Negative influencers Total :"+value
+    // console.log(negativeInfluencers)
+    return negativeInfluencers
+  })
+
+  let instagramDailyPositiveInfluencersText= [];
+  instagramDailyPositiveInfluencersText = props?.sortedInstagramDailyInfluencersStats?.instagramDailyPositiveInfluencers?.map((value,index)=>{
+    const negativeInfluencers="Positive influencers Total :"+value
+    // console.log(negativeInfluencers)
+    return negativeInfluencers
+  })
+
+  let bubbleSize = [];
+  bubbleSize = props?.sortedInstagramDailyInfluencersStats?.sortedInstagramDailyInfluencersDate?.map((value)=>{
+    return 200;
+  });
+  // console.log(bubbleSize)
+
+  let bubblePositiveInfluencersColor=[]
+  bubblePositiveInfluencersColor = props?.sortedInstagramDailyInfluencersStats?.sortedInstagramDailyInfluencersDate?.map((value)=>{
+    return "rgb(44, 160, 101)";
+  });
+  // console.log(bubblePositiveInfluencersColor)
+
+  let bubbleNegativeInfluencersColor=[]
+  bubbleNegativeInfluencersColor = props?.sortedInstagramDailyInfluencersStats?.sortedInstagramDailyInfluencersDate?.map((value)=>{
+    return "rgb(255, 65, 54)";
+  });
+  // console.log(bubbleNegativeInfluencersColor)
+
+  let bubbleNeutralInfluencersColor=[]
+  bubbleNeutralInfluencersColor = props?.sortedInstagramDailyInfluencersStats?.sortedInstagramDailyInfluencersDate?.map((value)=>{
+    return "rgb(255, 144, 14)";
+  });
+  // console.log(bubbleNeutralInfluencersColor)
+
+    
+  var trace1 = {
+    x: props.sortedInstagramDailyInfluencersStats.sortedInstagramDailyInfluencersDate,
+    y: props.sortedInstagramDailyInfluencersStats.instagramDailyNegativeInfluencers,
+    text: instagramDailyNegativeInfluencersText,
+    mode: 'markers',
+    marker: {
+      color:bubbleNegativeInfluencersColor,
+      size: bubbleSize,
+      sizemode: 'area'
+    }
+  };
+  
+  var trace2 = {
+    x: props.sortedInstagramDailyInfluencersStats.sortedInstagramDailyInfluencersDate,
+    y: props.sortedInstagramDailyInfluencersStats.instagramDailyNeutralInfluencers,
+    text: instagramDailyNeutralInfluencersText,
+    mode: 'markers',
+    marker: {
+      color:bubbleNeutralInfluencersColor,
+      size: bubbleSize,
+      //setting 'sizeref' to lower than 1 decreases the rendered size
+      // sizeref: 0.2,
+      sizemode: 'area'
+    }
+  };
+  
+  var trace3 = {
+    x: props.sortedInstagramDailyInfluencersStats.sortedInstagramDailyInfluencersDate,
+    y: props.sortedInstagramDailyInfluencersStats.instagramDailyPositiveInfluencers,    
+    text: instagramDailyPositiveInfluencersText,
+    mode: 'markers',
+    marker: {
+      color:bubblePositiveInfluencersColor,
+      size:bubbleSize,
+      //setting 'sizeref' to less than 1, increases the rendered marker sizes
+      // sizeref: 1,
+      sizemode: 'area'
+    }
+  };
+  
+  var data = [trace1, trace2, trace3];
+  
+  var layout = {
+    title: 'Instagram influencers stats',
+    showlegend: false,
+    height: 800,
+    width: 800
+  };
   return (
     <Plot
     data={data}
